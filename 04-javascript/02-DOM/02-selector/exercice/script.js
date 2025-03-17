7803f888a99a5a21b2598d4d5bae778b5c96f6c7
Nolwenn <nolwenn.wm@afci-formation.fr>
2025-03-17 11:45:24 +0100

    cours selecteur JS et exercice

---
+++ 04-javascript/02-DOM/02-selector/exercice/script.js
@@ -0,0 +1,9 @@
+// ------------EXO 1 ---------------
+// Déplacer la modale dans le body. Celle ci est déjà présente, mais le CSS la cache. Il faudra la déplacer sans modifier le CSS
+
+// ----------- EXO 2 ---------------
+// modifier le texte des 3 li du footer, si possible avec une boucle.
+
+
+// ------------ EXO 3 --------------
+// Remplacer le texte des paragraphes pair.
